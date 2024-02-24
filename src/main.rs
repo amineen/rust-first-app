2f415eea19f0ae27b21f6915af420bdcff9d808c
Aaron Mineen <amineen64@hotmail.com>
2024-02-24 21:27:15 +0200

    Add random number generation and guessing game logic

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,31 @@
+use rand::Rng;
+use std::cmp::Ordering;
 use std::io;
-fn main() {
-    let mut guess: String = String::new();
-    println!("Please guess a number.");
-    println!("Enter your guess: ");
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
 
-    println!("You guessed: {}", guess);
+fn main() {
+    println!("This is a guessing game!");
+    let secret_number = rand::thread_rng().gen_range(1..=10);
+    let mut guess: String = String::new();
+    loop {
+        guess.clear();
+        println!("Please guess a number: ");
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Please enter a valid number!");
+                continue;
+            }
+        };
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
 }
